b101ab4dc09ddcee2c74d97e2394c88bd04d4ecb
Samira <samiraak@code.edu.az>
2023-11-23 16:37:27 +0400

    Razor Pages

---
+++ Pages/about.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp.Pages.Shared
+{
+    public class aboutModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ namespace asp
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddRazorPages();
             var app = builder.Build();
 
             app.UseStaticFiles();
 
             app.MapGet("/", () => "Hello World!");
-            app.MapGet("/haha", () => "haha World!");
 
+            app.MapRazorPages();
             app.Run();
         }
     }
